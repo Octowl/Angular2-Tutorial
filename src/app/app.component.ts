05cb3287010e7e4d49179b428202dabce183b1b6
Octowl <alsaff1987@gmail.com>
2017-03-21 16:30:13 +0300

    Step8 - Putting our hero service to use

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'GDG Superheroes';
   heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+      this.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
       this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+      this.heroes = this.heroService.getHeroes();
   }
 }
