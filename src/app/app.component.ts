d656cd311c3d6ed90446edf4cdf7f888a63c476b
Octowl <alsaff1987@gmail.com>
2017-03-21 15:26:31 +0300

    Step3 - So many heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,20 @@ export class Hero {
     name: string;
 }
 
+const HEROES: Hero[] = [
+  { id: 1, name: 'Mr. M. M. Meer' },
+  { id: 2, name: 'Abdul The Hackinator' },
+  { id: 3, name: 'Burhan' },
+  { id: 4, name: 'Abdullah Droid Hunter' },
+  { id: 5, name: 'The Octowl' }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'GDG Superheros';
-  hero: Hero = {
-      id: 1,
-      name: 'Burhan'
-  }
+  title = 'GDG Superheroes';
+  heroes = HEROES;
 }
