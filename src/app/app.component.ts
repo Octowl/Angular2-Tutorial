f78c7969d1a321b10fa560fe545808f751bfb491
Octowl <alsaff1987@gmail.com>
2017-03-21 16:20:33 +0300

    Step7 - Heroes are here to serve

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
 
-const HEROES: Hero[] = [
-  { id: 1, name: 'Mr. M. M. Meer' },
-  { id: 2, name: 'Abdul The Hackinator' },
-  { id: 3, name: 'Burhan' },
-  { id: 4, name: 'Abdullah Droid Hunter' },
-  { id: 5, name: 'The Octowl' }
-];
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,7 +8,7 @@ const HEROES: Hero[] = [
 })
 export class AppComponent {
   title = 'GDG Superheroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
   onSelect(hero: Hero): void {
       this.selectedHero = hero;
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Mr. M. M. Meer' },
+  { id: 2, name: 'Abdul The Hackinator' },
+  { id: 3, name: 'Burhan' },
+  { id: 4, name: 'Abdullah Droid Hunter' },
+  { id: 5, name: 'The Octowl' }
+];
