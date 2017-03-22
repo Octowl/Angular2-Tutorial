1ca6a14db52a5714eaf28a0e25785d0d328a6442
Octowl <alsaff1987@gmail.com>
2017-03-22 15:23:38 +0300

    Step20 - Too many heroes. I delete you

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -4,9 +4,9 @@ export class InMemoryDataService implements InMemoryDbService {
     createDb() {
         let heroes = [
           { id: 1, name: 'Mr. M. M. Meer' },
-          { id: 2, name: 'Abdul The Hackinator' },
+          { id: 2, name: 'The Hackinator' },
           { id: 3, name: 'Burhan' },
-          { id: 4, name: 'Abdullah Droid Hunter' },
+          { id: 4, name: 'Droid Hunter' },
           { id: 5, name: 'The Octowl' }
       ];
       return {heroes};
